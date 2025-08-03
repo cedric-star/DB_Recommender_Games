30d410d777143685c886ca6a0968c21540c35dc9
Vicky <jakob@unfe.de>
2025-08-03 11:46:57 +0200

    Classes

--- out/artifacts/DB_Recommender_Games/WEB-INF/classes/servlets/LoginBean.java
+++ out/artifacts/DB_Recommender_Games/WEB-INF/classes/servlets/LoginBean.java
@@ -34,7 +34,7 @@ public class LoginBean extends HttpServlet {
 			sb.append("<a href=\"ControllerServlet?doAction=logout\">Logout</a>");
 		} else {
 			sb.append("<h1>Anmeldung</h1>");
-			sb.append("<p>Du brauchst noch einen Account? Hier geht es zur <a href=\"ControllerServlet?doAction=register\">Registrierung</a></p>"
+			sb.append("<p>Du brauchst noch einen Account? Hier geht es zur <a href=\"ControllerServlet?doAction=register\">Registrierung</a>.</p>"
 							+ "<br>"
 							+ "    	<br>"
 							+ "    	<FORM ACTION=\"ControllerServlet?doAction=login\" METHOD=\"POST\">"
